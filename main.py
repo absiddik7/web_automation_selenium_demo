d222d5a49714d8953c7e0af1222196691f70525e
absiddik7 <abubakkar1070@gmail.com>
2024-11-07 09:17:58 +0600

    updated

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     browser = "chrome"
 
     # Define headless mode - True or False
-    headless = False
+    headless = True
 
     # Update default browser setting
     update_default_browser(browser)
